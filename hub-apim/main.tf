76edd22c2d3368b45013eae9924ad564fd9d18cf
Rémi BUISSON <remi.buisson@traefik.io>
2025-07-11 12:04:51 +0200

    chore: update dependencies

--- hub-apim/main.tf
+++ hub-apim/main.tf
@@ -95,7 +95,7 @@ data "helm_template" "traefik" {
     }
   ]
 
-  # As of v3.0.0-pre2, the data source requires this kube_version to be set.
+  # As of v3.0.2, the data source requires this kube_version to be set.
   # Otherwise, it believes we are on k8s v1.20.
   kube_version = data.oci_containerengine_cluster.target.kubernetes_version
 }
